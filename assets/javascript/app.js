02a16dc1eab065055edc9869ef0688cff8a875d4
reb2039607 <reb.aguayo@gmail.com>
2019-06-21 00:24:58 -0700

    can't seem to fix the timer, all else should be complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,16 @@
-$("#start").on("click", function(){
+$("#start").on("click", function () {
     game.start();
 })
+
+$(document).on("click", "#end", function(){
+    game.done();
+});
 
 var questions = [{
     question: "Which of these is not a PowerPuff Girl?",
     answers: ["Blossom", "Bubbles", "Butterscotch", "Buttercup"],
     correctAnswer: "Butterscotch"
-},   {
+}, {
     question: "What do you call a group of cats?",
     answers: ["Flock", "Gang", "Litter", "Clowder"],
     correctAnswer: "Clowder"
@@ -28,28 +32,99 @@ var questions = [{
     correctAnswer: "Lasagna"
 }];
 
+// timer = setInterval(game.countdown, 1000); 
 const game = {
     correct: 0,
     incorrect: 0,
-    counter: 120,
-    countdown: function(){
+    counter: 10, //change back to 120
+    countdown: function () {
         game.counter--;
         $("#counter").html(game.counter);
-        if(game.counter===0){
-            alert("Time is up!");
+        if (game.counter == 0) {
+            alert("Time's up!");
             game.done();
+
         }
     },
-    start: function(){
-        timer = setInterval(game.countdown, 1000);
-        $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter'>120</span> Seconds")
-        $("#start").remove();
-    for(let i = 0; i<questions.length; i++) {
-        $("#subwrapper").append('<h2>' + questions[i].question + '</h2>');
+    start: function () {
 
-        for(let j = 0; j<questions[j].answers.length;j++){
-            $("#subwrapper").append("<input type = 'radio' name = 'question- "+ i +" ' value=' "+ questions[i].answers[j]+ "'>"+questions[i].answers[j])
+        timer = setInterval(game.countdown, 1000); 
+        $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter'>120</span> Seconds</h2>");
+        $("#start").remove();
+
+        for (let i = 0; i < questions.length; i++) {
+            $("#subwrapper").append('<h2>' + questions[i].question + '</h2>');
+
+            for (let j = 0; j < questions[i].answers.length; j++) {
+                $("#subwrapper").append("<input type = 'radio' name = 'question- " + i + " ' value=' " + questions[i].answers[j] + "'>" + questions[i].answers[j])
+
+            }
         }
-    }
-    }
+        $("#subwrapper").append("<br><button id='end'>Done!</button>");
+    },
+
+    done: function () {
+        
+        $.each($("input[name='question-0]':checked"), function() {
+            if($(this).val()==questions[0].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        $.each($("input[name='question-1]':checked"), function() {
+            if($(this).val()==questions[1].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        $.each($("input[name='question-2]':checked"), function() {
+            if($(this).val()==questions[2].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        $.each($("input[name='question-3]':checked"), function() {
+            if($(this).val()==questions[3].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        $.each($("input[name='question-4]':checked"), function() {
+            if($(this).val()==questions[4].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        $.each($("input[name='question-5]':checked"), function() {
+            if($(this).val()==questions[5].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        this.result();
+        },
+
+        result: function(){
+
+            clearInterval(timer);
+            $('#subwrapper h2').remove();
+
+            $("#subwrapper").html("<h2>Great Job!</h2>");
+            $("#subwrapper").append("<h3>Correct Answers: " + this.correct + "</h3>");
+            $("#subwrapper").append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+            $("#subwrapper").append("<h3>Unanswered: " + (questions.length-(this.incorrect + this.correct)) + "</h3>")
+
+        }
 }
